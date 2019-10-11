39b8dd765a9d259f3b5e3317801ffab0d1b428f2
Stanislaw Osinski <stanislaw.osinski@carrotsearch.com>
2019-10-11 09:03:11 +0200

    Simplify.

--- src/replace-variables.js
+++ src/replace-variables.js
@@ -1,17 +1,17 @@
-const VARIABLE_REGEX = /^\w+$/;
 const VARIABLE_WITH_DELIMITER_REGEX = /%(\w+)%/g;
-
 exports.replaceVariables = (html, replacer) => {
-  return html.replace(VARIABLE_WITH_DELIMITER_REGEX, function (match, name, offset) {
-    return replacer(name, offset);
-  });
+  return html.replace(VARIABLE_WITH_DELIMITER_REGEX, 
+    (match, name, offset) => replacer(name, offset));
 };
 
+const VARIABLE_REGEX = /^\w+$/;
 exports.validateVariables = variables => {
   // Validate variable names
-  const offending = Object.keys(variables).filter(v => !VARIABLE_REGEX.test(v));
+  const offending = Object.keys(variables)
+    .filter(v => !VARIABLE_REGEX.test(v));
   if (offending.length > 0) {
-    throw "Variable names must match [A-Za-z0-9_], offending names: " + offending.join(", ") + ".";
+    throw "Variable names must match [A-Za-z0-9_], offending names: "
+      + offending.join(", ") + ".";
   }
 };
 
